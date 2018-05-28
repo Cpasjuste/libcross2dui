99dd1a07a936d59db23bb20d7d2c692d24c5c156
cpasjuste <cpasjuste@gmail.com>
2018-05-28 11:52:00 +0200

    first commit

---
+++ include/c2dui.h
@@ -0,0 +1,38 @@
+//
+// Created by cpasjuste on 28/05/18.
+//
+
+#ifndef C2DUI_H
+#define C2DUI_H
+
+#ifdef __PSP2__
+#include <psp2/power.h>
+#include <psp2/io/dirent.h>
+
+int _newlib_heap_size_user = 192 * 1024 * 1024;
+#define SCR_W   960
+#define SCR_H   544
+#elif __PS3__
+#define SCR_W   1280
+#define SCR_H   720
+#elif __3DS__
+#define SCR_W   400
+#define SCR_H   240
+#elif __NX__
+#define SCR_W   1280
+#define SCR_H   720
+#else
+#define SCR_W   1280
+#define SCR_H   720
+#endif
+
+#define C2DUI_HOME_PATH "./"
+#define C2DUI_ROMS_PATHS_MAX 1
+
+#ifdef __PFBA__
+
+#elif __SNES9X__
+
+#endif
+
+#endif //C2DUI_H
