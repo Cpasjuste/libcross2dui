012f6b10d9e94ea43d6df93fd1a155dfe41f6866
david <cpasjuste@gmail.com>
2018-09-23 14:15:48 +0200

    switch: use title texture and font from sdcard

--- source/c2dui_skin.cpp
+++ source/c2dui_skin.cpp
@@ -4,19 +4,6 @@
 
 #include "c2dui.h"
 
-#ifdef __SWITCH__
-#ifdef __PFBA__
-#include "../../pfba/data/skin/title.h"
-#include "../../pfba/data/skin/default.h"
-#elif __PSNES__
-#include "../../psnes/data/skin/title.h"
-#include "../../psnes/data/skin/default.h"
-#elif __PNES__
-#include "../../pnes/data/skin/title.h"
-#include "../../pnes/data/skin/default.h"
-#endif
-#endif
-
 using namespace c2d;
 using namespace c2dui;
 
@@ -24,15 +11,11 @@ C2DUISkin::C2DUISkin(const std::string &p, const std::vector<Button> &btns) {
 
     this->path = p + "skin/";
 
-#ifdef __SWITCH__
-    tex_title = (Texture *) new C2DTexture((const unsigned char *) pfba_title, pfba_title_length);
-    font = new C2DFont();
-    font->loadFromMemory(pfba_font, pfba_font_length);
-#else
     tex_title = new C2DTexture((path + "title.png").c_str());
+    tex_title->setFiltering(C2D_TEXTURE_FILTER_POINT);
     font = new C2DFont();
     font->loadFromFile(path + "default.ttf");
-#endif
+    font->setFiltering(C2D_TEXTURE_FILTER_POINT);
     font->setYOffset(-5);
 
     // load buttons textures
