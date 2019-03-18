5326d0302cf3bdd4c7a94c79c4a931beaf3ee1c3
rsn8887 <rsn8887@users.noreply.github.com>
2019-03-18 14:55:47 -0500

    input: fix enabling/disabling single joycon mode on Switch

--- source/c2dui_ui_main.cpp
+++ source/c2dui_ui_main.cpp
@@ -184,11 +184,35 @@ void UIMain::updateInputMapping(bool isRomConfig) {
 
 #ifdef __SWITCH__
     int single_joy = config->get(Option::Index::JOY_SINGLEJOYCON)->getValueBool();
-    for (int i = 0; i < PLAYER_MAX; i++) {
+    if (!hidGetHandheldMode()) {
         if (single_joy > 0) {
-            hidSetNpadJoyAssignmentModeSingleByDefault((HidControllerID) i);
+            for (int id = 0; id < 8; id++) {
+                hidSetNpadJoyAssignmentModeSingleByDefault((HidControllerID) id);
+            }
+            hidSetNpadJoyHoldType(HidJoyHoldType_Horizontal);
+            hidScanInput();
         } else {
-            hidSetNpadJoyAssignmentModeDual((HidControllerID) i);
+            // find all left/right single JoyCon pairs and join them together
+            for (int id = 0; id < 8; id++) {
+                hidSetNpadJoyAssignmentModeDual((HidControllerID) id);
+            }
+            int lastRightId = 8;
+            for (int id0 = 0; id0 < 8; id0++) {
+                if (hidGetControllerType((HidControllerID) id0) & TYPE_JOYCON_LEFT) {
+                    for (int id1 = lastRightId - 1; id1 >= 0; id1--) {
+                        if (hidGetControllerType((HidControllerID) id1) & TYPE_JOYCON_RIGHT) {
+                            lastRightId = id1;
+                            // prevent missing player numbers
+                            if (id0 < id1) {
+                                hidMergeSingleJoyAsDualJoy((HidControllerID) id0, (HidControllerID) id1);
+                            } else if (id0 > id1) {
+                                hidMergeSingleJoyAsDualJoy((HidControllerID) id1, (HidControllerID) id0);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 #endif
